113d59a71a49f3d48cdfa5798c77aea38e89a6ab
alex-pythonista <indrajit.alex@gmail.com>
2023-01-08 19:54:48 +0600

    category data init

---
+++ lib/dummy_data.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+import './models/category.dart';
+
+const dummyCategories = [
+  Category(
+    id: 'c1',
+    title: 'Italiano',
+    color: Colors.green,
+  ),
+  Category(
+    id: 'c2',
+    title: 'Indian Cuisine',
+    color: Colors.orangeAccent,
+  ),
+  Category(
+    id: 'c3',
+    title: 'French Breakfast',
+    color: Colors.blueAccent,
+  ),
+  Category(
+    id: 'c4',
+    title: 'German',
+    color: Colors.black12,
+  ),
+  Category(
+    id: 'c5',
+    title: 'Chinese',
+    color: Colors.red,
+  ),
+  Category(
+    id: 'c6',
+    title: 'Japanese',
+    color: Colors.green,
+  ),
+  Category(
+    id: 'c7',
+    title: 'Korean',
+    color: Colors.orangeAccent,
+  ),
+  Category(
+    id: 'c8',
+    title: 'Turkish',
+    color: Colors.blueAccent,
+  ),
+  Category(
+    id: 'c9',
+    title: 'Fast Food',
+    color: Colors.black12,
+  ),
+  Category(
+    id: 'c10',
+    title: 'Bengali Special',
+    color: Colors.red,
+  ),
+];
